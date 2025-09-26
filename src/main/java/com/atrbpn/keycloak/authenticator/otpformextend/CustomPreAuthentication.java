aade058f193f7e2ec008bb6304a1d62343aadcd2
Sofyan Ardianto <sofyanard@gmail.com>
2025-09-26 13:21:57 +0700

    add tnc verifyUser :tiger:

--- src/main/java/com/atrbpn/keycloak/authenticator/otpformextend/CustomPreAuthentication.java
+++ src/main/java/com/atrbpn/keycloak/authenticator/otpformextend/CustomPreAuthentication.java
@@ -1,5 +1,10 @@
 package com.atrbpn.keycloak.authenticator.otpformextend;
 
+import java.io.IOException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.models.KeycloakSession;
@@ -8,14 +13,73 @@ import org.keycloak.models.UserModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.atrbpn.keycloak.authenticator.otpformextend.tnc.TncRequest;
+import com.atrbpn.keycloak.authenticator.otpformextend.tnc.TncResponse;
+import com.atrbpn.keycloak.authenticator.otpformextend.tnc.TncRestClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CustomPreAuthentication implements Authenticator {
 
     private static final Logger log = LoggerFactory.getLogger(CustomPreAuthentication.class);
 
+    private static String smtpHost;
+    private static String smtpFrom;
+    private static String environment;
+    private static String otpMechanism;
+    
+    static {
+        try {
+            Context initCxt =  new InitialContext();
+
+            smtpHost = (String) initCxt.lookup("java:/smtpHost");
+            smtpFrom = (String) initCxt.lookup("java:/smtpFrom");
+            environment = (String) initCxt.lookup("java:/environment");
+            otpMechanism = (String) initCxt.lookup("java:/otpMechanism");
+
+        } catch (Exception ex) {
+            log.error("unable to get jndi connection for SMTP or Environment");
+            log.error(ex.getMessage(), ex);
+        }
+    }
+
     @Override
     public void authenticate(AuthenticationFlowContext authenticationFlowContext) {
-        log.info("set pre-auth message!");
-        authenticationFlowContext.form().setAttribute("preAuthMessage", "pre-authentication message!");
+        
+        UserModel userModel = authenticationFlowContext.getUser();
+        String username = authenticationFlowContext.getAuthenticationSession().getAuthNote("username");
+        
+        // get tnc from external api here
+        if (TncRestClient.tncApiBaseUrl != null && !TncRestClient.tncApiBaseUrl.trim().isEmpty()) {
+            log.info("starting tnc request for username : {} ", username);
+            TncRequest tncRequest = new TncRequest(userModel.getAttributes().get("orcluserid").get(0), "internal");
+            TncResponse tncResponse;
+            try {
+                tncResponse = TncRestClient.verifyUser(tncRequest);
+                log.info("tnc response: {}", new ObjectMapper().writeValueAsString(tncResponse));
+
+                if (tncResponse != null)
+                {
+                    authenticationFlowContext.form().setAttribute("tncMessage", tncResponse.getMessage());
+                    if (tncResponse.getData() != null) {
+                        authenticationFlowContext.form().setAttribute("tncStatus", tncResponse.getData().getStatusTnc());
+                        authenticationFlowContext.form().setAttribute("tncContent", tncResponse.getData().getKonten());
+                        authenticationFlowContext.form().setAttribute("tncVersionUpdated", tncResponse.getData().getVersiTncTerbaru());
+                        authenticationFlowContext.form().setAttribute("tncUrl", tncResponse.getData().getUrl());
+
+                        // set statusTnc to auth note for next process
+                        String tncStatus = String.valueOf(tncResponse.getData().getStatusTnc());
+                        authenticationFlowContext.getAuthenticationSession().setAuthNote("tncStatus", tncStatus);
+                    } else {
+                        log.warn("tnc response data is null");
+                    }
+                } else {
+                    log.warn("tnc response is null");
+                }
+            } catch (IOException ex) {
+                log.error("error request tnc from external api");
+                log.error(ex.getMessage(), ex);
+            }
+        }
 
         authenticationFlowContext.success();
     }
---
+++ src/main/java/com/atrbpn/keycloak/authenticator/otpformextend/tnc/TncResponse.java
@@ -0,0 +1,71 @@
+package com.atrbpn.keycloak.authenticator.otpformextend.tnc;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TncResponse {
+
+    @JsonProperty("success")
+    private boolean success;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("data")
+    private TncResponseData tncResponseData;
+
+    // Getters and Setters
+
+    public static class TncResponseData {
+
+        @JsonProperty("USERID")
+        private String userId;
+
+        @JsonProperty("USERNAME")
+        private String username;
+
+        @JsonProperty("REALM")
+        private String realm;
+
+        @JsonProperty("TNC")
+        private String tnc;
+
+        @JsonProperty("VERSI_TNC_USER")
+        private String versiTncUser;
+
+        @JsonProperty("TNC_TERBARU_ID")
+        private String tncTerbaruId;
+
+        @JsonProperty("VERSI_TNC_TERBARU")
+        private String versiTncTerbaru;
+
+        @JsonProperty("TANGGALTNC")
+        private String tanggaltnc;
+
+        @JsonProperty("KONTEN")
+        private String konten;
+
+        @JsonProperty("URL")
+        private String url;
+
+        @JsonProperty("STATUS_TNC")
+        private int statusTnc;
+
+        // Getters and Setters
+        public String getUserId() { return userId; }
+        public String getUsername() { return username; }
+        public String getRealm() { return realm; }
+        public String getTnc() { return tnc; }
+        public String getVersiTncUser() { return versiTncUser; }
+        public String getTncTerbaruId() { return tncTerbaruId; }
+        public String getVersiTncTerbaru() { return versiTncTerbaru; }
+        public String getTanggaltnc() { return tanggaltnc; }
+        public String getKonten() { return konten; }
+        public String getUrl() { return url; }
+        public int getStatusTnc() { return statusTnc; }
+    }
+
+    public boolean isSuccess() { return success; }
+    public String getMessage() { return message; }
+    public TncResponseData getData() { return tncResponseData; }
+
+}
